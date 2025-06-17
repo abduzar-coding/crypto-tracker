55677552d066cab24322c3825a7e7166f759c9fb
Abduzar <abduzar.web@gmail.com>
2025-06-17 23:43:54 +0500

    Footer was added

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,18 @@ import CoinDetails from './pages/CoinDetails';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-gray-900 to-black text-white">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/coin/:id" element={<CoinDetails />} />
-      </Routes>
+    <div className="min-h-screen bg-gradient-to-br from-gray-900 to-black text-white flex flex-col">
+      <div className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/coin/:id" element={<CoinDetails />} />
+        </Routes>
+      </div>
+
+      {/* Footer */}
+      <footer className="text-center text-sm text-white/50 py-6">
+        Built by <span className="text-white font-semibold">Abduzar Khabib</span> • 2025
+      </footer>
     </div>
   );
 }
