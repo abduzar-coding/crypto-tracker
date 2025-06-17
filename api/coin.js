57881a8b7c61615310ef9a971ff250f493179bdd
Abduzar <abduzar.web@gmail.com>
2025-06-17 22:08:53 +0500

    🔥 Route all CoinGecko traffic through Vercel API to fix CORS and rate limits

--- api/coin.js
+++ api/coin.js
@@ -1,14 +1,20 @@
 export default async function handler(req, res) {
-  const { id, chart } = req.query;
+  const { id, chart, ...rest } = req.query;
 
-  const url = chart
-    ? `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=7`
-    : `https://api.coingecko.com/api/v3/coins/${id}`;
+  let url;
+
+  if (id === 'markets') {
+    const params = new URLSearchParams(rest).toString();
+    url = `https://api.coingecko.com/api/v3/coins/markets?${params}`;
+  } else if (chart) {
+    url = `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=7`;
+  } else {
+    url = `https://api.coingecko.com/api/v3/coins/${id}`;
+  }
 
   try {
     const response = await fetch(url);
     if (!response.ok) throw new Error('CoinGecko error');
-
     const data = await response.json();
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.status(200).json(data);
