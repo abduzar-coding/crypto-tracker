b17b2b1dd53f325e07ba826073a3d19e97fc6139
Abduzar <abduzar.web@gmail.com>
2025-06-17 10:48:05 +0500

    Initial commit for Crypto Tracker

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-500 text-white">
+      <h1 className="text-4xl font-bold">Crypto Tracker 🪙</h1>
+    </div>
+  );
+}
+
+export default App
